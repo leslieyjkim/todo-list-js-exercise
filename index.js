01700c73275d197d12fc600b765799585ea9fb6f
yoonjookim <yjblossom@gmail.com>
2023-11-24 16:38:32 -0500

    object oriented modified

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+    logState: function () {
+      console.log(
+        `${task.title} has${task.complete ? " " : " not "}been completed`
+      );
+    },
+
+    markCompleted: function () {
+      this.complete = true;
+    },
+  };
+  return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+//#1.
+// function logTaskState(task) {
+//   console.log(
+//     `${task.title} has${task.complete ? " " : " not "}been completed`
+//   );
+// }
+// modify the above functions into Method, and gonna put it into the object above
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+// #2.
+// function completeTask(task) {
+//   task.complete = true;
+// }
+// modify the above functions into Method, and gonna put it into the object above
 
-// DRIVER CODE BELOW
+const task1 = newTask(
+  "Clean Cat Litter",
+  "Take all the 💩 out of the litter box"
+);
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+// logTaskState(task1); // Clean Cat Litter has not been completed
+// completeTask(task1);
+// logTaskState(task1); // Clean Cat Litter has been completed
+// already modified the function into Method above, so we modified driver code also.
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+task1.logState();
+task1.markCompleted();
+task1.logState();
+
+console.log(tasks);
